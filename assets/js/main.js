b352a766fcae996f8a449babb3f4e625af142dae
Isabel Pereira <isabelalvaro116@gmail.com>
2023-05-12 18:26:16 -0300

    inicial portifolio project

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+function menuToggle(){
+  const menu = document.querySelector('#menu-icon')
+  const navbar = document.querySelector('#nav-bar')
+  menu.classList.toggle('bx-x')
+  navbar.classList.toggle('active')
+  }
